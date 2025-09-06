f3a642559c08db3d7fa3b86b1000347923b8cdc7
krrish6130 <krushnasanatani@gmail.com>
2025-09-07 01:39:20 +0530

    Update lineage_veux.mk

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common yaap stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
