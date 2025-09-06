076ddef3472f78ee2fc9549bdece540cb6fa42ea
krrish6130 <krushnasanatani@gmail.com>
2025-09-07 01:45:13 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# QCOM
+TARGET_BOARD_PLATFORM := holi
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
