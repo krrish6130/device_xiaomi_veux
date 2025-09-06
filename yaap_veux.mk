56bb54d76cde401ef689d004621f0ef8c3828e7b
krrish6130 <krushnasanatani@gmail.com>
2025-09-06 09:54:43 +0530

    Update yaap_veux.mk

--- yaap_veux.mk
+++ yaap_veux.mk
@@ -14,6 +14,12 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit some common yaap stuff.
 $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
+# Whether you are compiling being an OFFICIAL Maintainer:
+WITAQUA_BUILD_TYPE := UNOFFICIAL
+
+# Maintainer Name
+WITAQUA_MAINTAINER := "krrish9130"
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
